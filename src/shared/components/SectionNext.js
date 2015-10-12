73626f27a846c077436616f88ac6e0bd88232458
Peter Mouland <peter.mouland@gmail.com>
2015-10-12 16:59:50 +0100

    add comparators to referrers

--- src/shared/components/SectionNext.js
+++ src/shared/components/SectionNext.js
@@ -14,12 +14,28 @@ function mapTypes(name, data){
 }
 
 function merge(name, data, comparatorData, comparatorLabel){
-  comparatorData.forEach(function(cData){
-    if (cData[name] === data[name]){
-      data[comparatorLabel] = cData[articleLabel]
+  let merged = data.map((d) => {
+    let i=0
+    while (comparatorData.length && i < comparatorData.length) {
+      var cData = comparatorData[i]
+      if (cData[name] === d[name]) {
+        d[comparatorLabel] = cData[articleLabel]
+        comparatorData.splice(i, 1);
+        break
+      } else {
+        i++
+      }
     }
+    return d;
+  });
+  comparatorData.forEach(function(cData) {
+    merged.push({
+      [comparatorLabel] : cData[articleLabel],
+      [name]: cData[name],
+      [articleLabel]: 0
+    });
   })
-  return data;
+  return merged
 }
 
 function renameDataKey(data){
@@ -46,7 +62,7 @@ export default class SectionWhere extends React.Component {
     let bounce = data.is_last_page.map(renameDataKey);
     if (comparatorData.is_last_page){
       comparatorBounce = comparatorData.is_last_page.map(renameDataKey)
-      bounce = bounce.map((d) => merge(articleColumn, d, comparatorBounce, comparatorLabel))
+      bounce = merge(articleColumn, bounce, comparatorBounce, comparatorLabel)
       keys.push(comparatorLabel)
     }
 
--- src/shared/components/SectionReferrers.js
+++ src/shared/components/SectionReferrers.js
@@ -4,25 +4,50 @@ import Row from 'react-bootstrap/lib/Row';
 import BarChart from '../components/BarChart.js';
 import Table from '../components/Table';
 
+const referrerLabel = 'Views'
+const referrerColumn = 'referrer'
+
 function mapTypes (name, data){
   return {
     [name]: data[0],
-    views: data[1]
+    [referrerLabel]: data[1]
   };
 }
 
-function getReferrerUrls (data, i){
+function merge(name, data, comparatorData, comparatorLabel) {
+  let merged = data.map((d) => {
+    let i = 0
+    while (comparatorData.length && i < comparatorData.length) {
+      var cData = comparatorData[i]
+      if (cData[name] === d[name]) {
+        d[comparatorLabel] = cData[referrerLabel]
+        comparatorData.splice(i, 1);
+        break
+      } else {
+        i++
+      }
+    }
+    return d;
+  });
+  comparatorData.forEach(function (cData) {
+    merged.push({
+      [comparatorLabel]: cData[referrerLabel],
+      [name]: cData[name],
+      [referrerLabel]: 0
+    });
+  })
+  return merged
+}
+
+function getReferrerUrls(data, i) {
   const maxLen = 60;
-  const displayString = data[0].length > maxLen ? data[0].substr(0, maxLen)+'…' : data[0];
-  let url = displayString.indexOf('http')<0 ? displayString : (
-    <a target="_blank" href={data[0]} >
+  const displayString = data[0].length > maxLen ? data[0].substr(0, maxLen) + '…' : data[0];
+  let url = displayString.indexOf('http') < 0 ? displayString : (
+    <a target="_blank" href={data[0]}>
       {displayString}
     </a>
   );
-  return {
-    referrer: url,
-    views: data[1]
-  };
+  return mapTypes(referrerColumn, [url, data[1]])
 }
 
 export default class SectionReferrers extends React.Component {
@@ -38,16 +63,34 @@ export default class SectionReferrers extends React.Component {
 
     let data = this.props.data;
     let comparatorData = this.props.comparatorData;
-    let refs = data.referrer_types.map((data) => mapTypes('referrer', data));
-    let socialrefs = data.social_referrers.sort().map((data) => mapTypes('referrer', data));
+    let comparatorLabel = comparatorData.comparator + ' Average';
+    let keys = [referrerLabel]
+    if (comparatorData.comparator) {
+      keys.push(comparatorLabel)
+    }
+
     let refUrls = data.referrer_urls.map(getReferrerUrls);
 
+    let refs = data.referrer_types.map((data) => mapTypes(referrerColumn, data));
+    if (comparatorData.referrer_types) {
+      let comparatorRefTypes = comparatorData.referrer_types.map((data) => mapTypes(referrerColumn, data));
+      refs = merge(referrerColumn, refs, comparatorRefTypes, comparatorLabel)
+    }
+
+    let socialRefs = data.social_referrers.sort().map((data) => mapTypes(referrerColumn, data));
+    if (comparatorData.social_referrers) {
+      let comparatorSocial = comparatorData.social_referrers.map((data) => mapTypes(referrerColumn, data));
+      socialRefs = merge(referrerColumn, socialRefs, comparatorSocial, comparatorLabel)
+    }
+
     return (<div>
+
       <Row>
         <Col xs={12}>
           <h4>Where did the users come from?</h4>
         </Col>
       </Row>
+
       <Row>
         <Col xs={12}>
           <h5>External Sources</h5>
@@ -58,19 +101,19 @@ export default class SectionReferrers extends React.Component {
           <h6>Referrer Types</h6>
           <BarChart
             data={refs}
-            keys={['views']}
-            category={'referrer'}
+            keys={keys}
+            category={referrerColumn}
             yLabel="Page Views"
             xLabel="Referrer"
-          />
+            />
           <h6>Social Networks</h6>
           <BarChart
-            data={socialrefs}
-            keys={['views']}
-            category={'referrer'}
+            data={socialRefs}
+            keys={keys}
+            category={referrerColumn}
             yLabel="Page Views"
             xLabel="Social Network"
-          />
+            />
         </Col>
         <Col xs={12} sm={6}>
           <h6>Top URLs</h6>
