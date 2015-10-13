8bc6bfa15721bdac207a8e49a0304755e440e316
jonathan-best <jonathan.best@ft.com>
2015-10-13 13:17:58 +0100

    Add comparator formatter tests

---
+++ test/formatters/Comparator.spec.js
@@ -0,0 +1,80 @@
+import {expect} from 'chai';
+import ArticleFormat from '../../src/server/formatters/Articles';
+import ESArticleResults from '../fixtures/data/article_results';
+
+describe('Article Formatter', function() {
+  it('should be a function', function () {
+    expect(ArticleFormat).to.be.a.function;
+  });
+  it('should return a promise', function() {
+    let p = ArticleFormat();
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat(null);
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat(undefined);
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat(123);
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat('whats up');
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat([]);
+    expect(p.then).to.be.a.function;
+    p = ArticleFormat({});
+    expect(p.then).to.be.a.function;
+  });
+  it('should reject a promise with a MalformedArgumentsError if passed a string', function(done) {
+    ArticleFormat('donald trump')
+      .catch((error) => {
+        expect(error.name).to.equal('MalformedArgumentsError');
+        expect(error.data).to.equal('donald trump');
+        done();
+      });
+  });
+  it('should reject the promise with a MalformedArgumentsError if passed an object', function(done) {
+    ArticleFormat({})
+      .catch((error) => {
+        expect(error.name).to.equal('MalformedArgumentsError');
+        done();
+      });
+  });
+  it('should parse results correctly', function(done) {
+    ArticleFormat(ESArticleResults)
+      .then((data) => {
+        expect(data.article).to.be.a.object;
+        const props = [
+          'title',
+          'uuid',
+          'author',
+          'published',
+          'published_human',
+          'pageViews',
+          'timeOnPage',
+          'socialReaders',
+          'readTimes',
+          'genre',
+          'sections',
+          'topics',
+          'channels',
+          'referrer_types',
+          'referrer_names',
+          'referrer_urls',
+          'social_referrers',
+          'devices',
+          'countries',
+          'regions',
+          'is_last_page',
+          'user_cohort',
+          'rfv_cluster',
+          'is_first_visit'
+        ];
+        for (let i = 0; i < props.length; i++){
+          expect(data.article.hasOwnProperty(props[i])).to.be.truthy;
+        }
+        done();
+      })
+      .catch((error) => {
+        console.error('error', error);
+        done(error);
+      });
+  });
+});
