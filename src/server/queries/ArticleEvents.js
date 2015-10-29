754bea206b53adb25c51848b07144881f199f918
Peter Mouland <peter.mouland@gmail.com>
2015-10-29 11:17:39 +0000

    refactor queries

--- src/server/queries/ArticleEvents.js
+++ src/server/queries/ArticleEvents.js
@@ -1,50 +1,15 @@
 import assert from 'assert';
-import moment from 'moment';
+import * as build from './queryBuilder'
 
-export default function PageEventsQuery(query) {
+export default function ArticleEventsQuery(query) {
 
   assert.equal(typeof query, 'object',
     "argument 'query' should be an object");
 
-  assert.equal(typeof query.uuid, 'string',
-    "argument 'query' must contain a 'uuid' string property");
-
-  assert.equal(typeof query.dateFrom, 'string',
-    "argument 'query' must contain a 'dateFrom' date string property");
-
-  assert.equal(typeof query.dateTo, 'string',
-    "argument 'query' must contain a 'dateTo' date property");
-
-  let match =  {  article_uuid: query.uuid  }
-  let filter = {
-    "and" : [
-      {
-        range : {
-          event_date : {
-            from: query.dateFrom,
-            to: query.dateTo
-          }
-        }
-      }
-    ]
-  }
-  for (var o in query.filters){
-    if (query.filters[o]){
-      filter.and.push({
-        "term" : { [o]: query.filters[o] }
-      })
-    }
-  }
+  let articleQuery = build.articleQuery(query)
 
   return {
-    query : {
-      filtered : {
-        query : {
-          match : match
-        },
-        filter : filter
-      }
-    },
+    query : articleQuery,
     "size": 1,
     "aggs" : {
       social_shares : {
