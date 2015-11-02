824ba375ac4b45e01edab13331c148d91b28b0a7
Peter Mouland <peter.mouland@gmail.com>
2015-11-02 14:05:51 +0000

    test the article view updating the queries

--- test/handlers/articleView.spec.js
+++ test/handlers/articleView.spec.js
@@ -8,26 +8,47 @@ import SectionModifier from '../../src/shared/components/SectionModifier';
 import Logo from '../../src/shared/components/Logo';
 import * as formatAuthors from '../../src/shared/utils/formatAuthors';
 import ArticleStore from '../../src/shared/stores/ArticleStore';
+import ComparatorQueryStore from '../../src/shared/stores/ComparatorQueryStore';
+import ArticleQueryStore from '../../src/shared/stores/ArticleQueryStore';
+import ArticleQueryActions from '../../src/shared/actions/ArticleQueryActions';
+import ComparatorQueryActions from '../../src/shared/actions/ComparatorQueryActions';
 
 const TestUtils = React.addons.TestUtils;
 
 describe ('ArticleView Handler', function() {
   let stub;
+  let queryStub;
   let formatStub;
+  let actionStub;
+  let compActionStub;
+  let compQueryStub;
+  let compActionSelectStub;
 
   beforeEach(function () {
     stub = sinon.stub(ArticleStore, 'getState');
-     formatStub = sinon.stub(formatAuthors, 'join');
-
+    queryStub = sinon.stub(ArticleQueryStore, 'getState');
+    formatStub = sinon.stub(formatAuthors, 'join');
+    actionStub = sinon.spy(ArticleQueryActions, 'setUUID');
+    compActionStub = sinon.spy(ComparatorQueryActions, 'setUUID');
+    compActionSelectStub = sinon.spy(ComparatorQueryActions, 'selectComparator');
+    compQueryStub = sinon.stub(ComparatorQueryStore, 'getState');
   });
+
   afterEach(function () {
     stub.restore();
     formatStub.restore();
+    queryStub.restore();
+    actionStub.restore();
+    compActionStub.restore();
+    compQueryStub.restore();
+    compActionSelectStub.restore();
   });
 
   it ('Should render component', function() {
     formatStub.returns('');
     stub.returns({ data:{ title:'test me' } });
+    queryStub.returns({ query:{ uuid: null } });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
     let articleView = createAltWrappedComponent(ArticleView, {
       params:{}
     });
@@ -44,6 +65,8 @@ describe ('ArticleView Handler', function() {
   it ('Should render a loading message', function() {
     formatStub.returns('');
     stub.returns({ });
+    queryStub.returns({ query:{ uuid: null } });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
     let articleView = createAltWrappedComponent(ArticleView, {
       params:{}
     });
@@ -58,6 +81,8 @@ describe ('ArticleView Handler', function() {
   it ('Should render a error message', function() {
     formatStub.returns('');
     stub.returns({ data:{ }, errorMessage:'fooBar' });
+    queryStub.returns({ query:{ uuid: null } });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
     let articleView = createAltWrappedComponent(ArticleView, {
       params:{}
     });
@@ -66,6 +91,60 @@ describe ('ArticleView Handler', function() {
 
     expect(error404.props.title).to.equal('Lantern - Article Not Found');
     expect(TestUtils.isElementOfType(error404, Error404)).to.equal(true);
+
+  });
+
+  it ('Should not set uuid\'s if they are the same', function() {
+    formatStub.returns('');
+    stub.returns({ data:{ } });
+    queryStub.returns({ query:{ uuid:'ive-not-changed'} });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
+    createAltWrappedComponent(ArticleView, {
+      params:{uuid:'ive-not-changed'}
+    });
+
+    expect(actionStub.calledOnce).to.equal(false);
+    expect(compActionStub.calledOnce).to.equal(false);
+
+  });
+
+  it ('Should set uuid\'s if they have changed', function() {
+    formatStub.returns('');
+    stub.returns({ data:{ } });
+    queryStub.returns({ query:{ uuid:'ive-changed'} });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
+    createAltWrappedComponent(ArticleView, {
+      params:{uuid:'uuid-me-up'}
+    });
+
+    expect(actionStub.calledOnce).to.equal(true);
+    expect(compActionStub.calledOnce).to.equal(true);
+
+  });
+
+  it ('Should not set comparator if it is the same', function() {
+    formatStub.returns('');
+    stub.returns({ data:{ } });
+    queryStub.returns({ query:{  }});
+    compQueryStub.returns({ query:{ comparator: 'ive-not-changed' } });
+    createAltWrappedComponent(ArticleView, {
+      params:{comparator:'ive-not-changed'}
+    });
+
+    expect(compActionSelectStub.calledOnce).to.equal(false);
+
+  });
+
+  it ('Should set comparator if it has changed', function() {
+    formatStub.returns('');
+    stub.returns({ data:{ } });
+    queryStub.returns({ query:{ } });
+    compQueryStub.returns({ query:{ comparator: 'test-comp' } });
+    createAltWrappedComponent(ArticleView, {
+      params:{comparator:'ive-not-changed'}
+    });
+
+    expect(compActionSelectStub.calledOnce).to.equal(true);
 
   });
 });
