b299594ac740a3043fb48c3d4995f89ba8b05930
jonathan-best <jonathan.best@ft.com>
2015-12-03 10:17:20 +0000

    Add placeholder tests to remind us!

--- test/utils/formatData.spec.js
+++ test/utils/formatData.spec.js
@@ -8,7 +8,7 @@ describe ('#formatData', function() {
   let dataFormatter;
 
   before(function() {
-    dataFormatter = new FormatData(ESArticleResults, ESArticleComparatorResults);
+    dataFormatter = new FormatData(ESArticleResults.aggregations, ESArticleComparatorResults.aggregations);
   });
 
   it ('Should set up the constructor', function() {
@@ -16,5 +16,23 @@ describe ('#formatData', function() {
     expect(dataFormatter.hasOwnProperty('COMPARATORDATA')).to.equal(true);
     expect(dataFormatter.hasOwnProperty('id')).to.equal(true);
   });
+
+  it ('mapTypes', function(){
+    expect(dataFormatter.mapTypes([ 'search', 60991 ])).to.deep.equal({ category: 'search', undefined: 60991 });
+    expect(dataFormatter.mapTypes([ 'T', 60991 ], 'True', 'False')).to.deep.equal({ category: 'True', undefined: 60991 })
+    expect(dataFormatter.mapTypes([ 'F', 60991 ], 'True', 'False')).to.deep.equal({ category: 'False', undefined: 60991 })
+  })
+
+  xit('merge', function(){
+
+  })
+
+  xit('getMetric', function(){
+
+  })
+
+  xit('getPCTMetric', function(){
+
+  })
 
 });
