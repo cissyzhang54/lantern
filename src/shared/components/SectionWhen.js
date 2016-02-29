0d4a976068390444542a0ad57718de2ed897e2b3
Dan Forys <Daniel.Forys@ft.com>
2016-02-29 13:07:22 +0000

    Check for existant timeData before attempting to set a property on it

--- src/shared/components/SectionWhen.js
+++ src/shared/components/SectionWhen.js
@@ -34,7 +34,7 @@ export default class SectionWhen extends React.Component {
 
     // Update the first data point to match the publish time
     // as the aggs are in hours not minutes
-    if(this.props.lastPublishDates.length > 0) {
+    if(this.props.lastPublishDates.length > 0 && timeData[0]) {
       timeData[0].category = this.props.lastPublishDates[0].key_as_string
     }
 
